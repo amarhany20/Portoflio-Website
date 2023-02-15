9235633666973207315659be5f785bba8c92a5e7
Ammar Hany Ezeldin Abdelrazik <ammarhanyezeldin@gmail.com>
2023-02-15 16:36:34 +0300

    fixed navbar in darkmode, small changes

--- scripts.js
+++ scripts.js
@@ -1,21 +1,35 @@
-
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-    $(".navbar-collapse").collapse("hide");
-  } else {
-    document.getElementById("navbar").style.top = "-70px";
-    $(".navbar-collapse").collapse("hide");
-    
-  }
-  prevScrollpos = currentScrollPos;
-};
+
+var path = window.location.pathname;
+var page = path.split("/").pop();
+if (page == "index.html") {
+  window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbar").style.top = "0";
+      $(".navbar-collapse").collapse("hide");
+    } else {
+      document.getElementById("navbar").style.top = "-70px";
+      $(".navbar-collapse").collapse("hide");
+    }
+    prevScrollpos = currentScrollPos;
+  };
+}
+else if(page == "home-dark.html") {
+   window.onscroll = function () {
+     var currentScrollPos = window.pageYOffset;
+     if (prevScrollpos > currentScrollPos) {
+       document.getElementById("navbar-dark").style.top = "0";
+       $(".navbar-collapse").collapse("hide");
+     } else {
+       document.getElementById("navbar-dark").style.top = "-70px";
+       $(".navbar-collapse").collapse("hide");
+     }
+     prevScrollpos = currentScrollPos;
+   };
+}
 
 /* ----------------------- contact-section ----------------------- */
 
 // Auto Collabse on scroll on sml screens
-
-
